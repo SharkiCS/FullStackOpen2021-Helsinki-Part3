83be72cee2f04206ff9ac8eef4c9b0d6e643bdf2
Sharki <Sharkismanagements@gmail.com>
2021-08-18 15:40:20 +0200

    Steps 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+
 app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms - :res[content-length] :body '))
 
 let persons = [
     {
@@ -25,6 +30,7 @@ let persons = [
         "id": 4
     }
 ]
+
 
 
 app.get('/info', (request, response) => {
